7b0ca84dba61e389c8648dfe43ceb991f6d5c618
ejaakml <eximindotong@yahoo.com>
2020-11-07 16:50:54 +0700

    39

---
+++ src/pboif2/pkg10119061/latihan39/nilaiterbesardanterkecil/Nilai.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.latihan39.nilaiterbesardanterkecil;
+import java.util.Scanner;
+/**
+ *
+ * @author Rheiza
+ */
+class Nilai {
+     Scanner scanner = new Scanner(System.in);
+    int[]nilai = new int [10];
+    int n;
+    int max;
+    int min;
+    
+    public int inputBanyakNilai() {
+        System.out.print("Masukkan Banyaknya Nilai Mahasiswa : ");
+        n = scanner.nextInt();
+        return n;
+    }
+    
+    public void inputNilai() {
+        for (int i = 1; i <=n; i++) {
+            System.out.print("Nilai Mahasiswa ke-" + i + "=");
+            nilai[i] = scanner.nextInt();
+        }
+    }
+    
+    public void outputNilai() {
+        for (int i = 1; i <=n; i++) {
+            System.out.println("Nilai Mahasiswa ke-" + i + " = " + nilai[i]);
+        }
+    }
+    
+    public int max () {
+        max=nilai[1];
+        for (int i = 1; i <=n; i++) {
+            if (nilai[i] > max) {
+                max = nilai[i];
+            }
+        }
+        return max;
+    }
+    
+    public int min () {
+        min=nilai[1];
+        for (int i = 1; i <=n; i++) {
+            if (nilai[i] < min) {
+                min=nilai[i];
+            }
+        }
+        return min;
+    }
+    
+}
---
+++ src/pboif2/pkg10119061/latihan39/nilaiterbesardanterkecil/PBOIF210119061Latihan39NilaiTerbesardanTerkecil.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.latihan39.nilaiterbesardanterkecil;
+import java.util.Scanner;
+/**
+ *
+ * @author Rheiza
+ */
+public class PBOIF210119061Latihan39NilaiTerbesardanTerkecil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+           Nilai nilai = new Nilai();
+        Scanner scanner = new Scanner(System.in);
+        String nama;
+        
+        System.out.println("======Program Terbesar dan Terkecil Nilai Mahasiswa======");
+        System.out.print("Masukkan Nama Petugas : ");
+        nama = scanner.nextLine();
+        nilai.inputBanyakNilai();
+        nilai.inputNilai();
+        System.out.println();
+        System.out.println("Hasil Nilai Mahasiswa");
+        nilai.outputNilai();
+        System.out.println("");
+        System.out.println("Nilai Terbesar adalah : " + nilai.max());
+        System.out.println("Nilai Terkecil adalah : " + nilai.min());
+        System.out.println("");
+        System.out.println("Petugas : " + nama);
+    }
+    
+}
